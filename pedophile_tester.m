a3be633579c690a8a4c9ec64db8ba3c0fbe7708c
howemeisterr <59666967+howemeisterr@users.noreply.github.com>
2020-01-09 16:44:38 -0700

    Create pedophile_tester.m

---
+++ pedophile_tester.m
@@ -0,0 +1,11 @@
+yage = input('your age?')
+tage = input('their age?')
+if yage > tage
+    age = yage / 2
+    age = age + 7
+    tage >= age
+elseif tage > yage
+    age = tage/2
+    age = age +7
+    yage >= age
+end
